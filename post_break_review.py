ed2dc48a4fc07fd0437775771d0b85a379e4777a
mfcust <43455391+mfcust@users.noreply.github.com>
2021-01-04 12:21:47 -0800

    Create post_break_review.py

---
+++ post_break_review.py
@@ -0,0 +1,32 @@
+# 1) Print Hello world to the console:
+
+
+
+#2) Calculate 1000 multiplied by 1000 and then print the answer to the console:
+
+
+
+
+#3) Create a string value for your first name and a string value for your last name, add them together and then print to the console. Make sure there is a space between your first and last name!
+
+
+
+
+#4) Use the input function to ask the user a question, and store their answer in a variable. Then, use their answer in a print function:
+#Example: Ask the user what their name is, and then print "Hi (name), nice to meet you!"
+
+
+
+#5a) Create a list of 5 string values:
+
+
+
+#5b) Print one of the items in the list by indexing it:
+
+
+
+#5c) Append an item to your list using the .append() method so it has 6 items. Print your new list:
+
+
+
+#5d) Delete the second item in your list by using the del function. Print your new list:
